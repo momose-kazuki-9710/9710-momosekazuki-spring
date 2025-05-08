e45a95c64eb0aedfaeb1383b6139d8e036cae2b2
momose-kazuki-9710 <600-student011@la-study.com>
2025-05-08 17:25:50 +0900

    R7_05_08_DB(登録・更新・削除)

--- spring-kadai6-jpa-crud/src/main/java/com/example/demo/controller/ItemController.java
+++ spring-kadai6-jpa-crud/src/main/java/com/example/demo/controller/ItemController.java
@@ -163,6 +163,13 @@ public class ItemController {
 		return "redirect:/items";
 	}
 	
+	
+	
+	
+	
+	
+	
+	
 	@GetMapping("/categories")
 	public String categoriesIndex(Model model) {
 		
@@ -173,6 +180,115 @@ public class ItemController {
 		return "categories";
 	}
 	
+	@GetMapping("/categories/add")
+	public String categoriesCreate() {
+		return "addCategories";
+	}
+	
+	//＝＝ 登録 ＝＝
+		//登録ボタンを押したとき、itemsテーブルにデータを登録する
+		// ＊一連の流れ＊
+			//1. 入力した内容の受け取り
+			//2. エラーチェック
+			//3. 入力した内容をDBに登録する
+			//4. 商品一覧の画面を開く
+				// (itemsテーブルのデータを取得して、items.htmlを開く)
+	@PostMapping("/categories/add")
+	public String categoriesStore(
+			@RequestParam(name="name") String name
+				) {
+			//3.
+			Categories categories = new Categories(name);
+			categoriesRepository.save(categories);
+			
+			//4.
+			return "redirect:/categories";
+		}
+	//＝＝ 更新準備 ＝＝
+		//更新ボタンを押したとき、更新のためのitemsテーブルにデータを表示する
+		// ＊一連の流れ＊
+			//1. 更新対象のデータを取得
+			//2. 取得できなかった場合、商品一覧の画面に戻る
+			//3. 取得したデータをhtmlで扱えるようにする
+			//4. 更新画面を開く
+	@GetMapping("/categories/{id}/edit")
+	public String categoriesEdit(
+			@PathVariable(name="id")Integer id,
+			Model model) {
+			
+		//1.
+		Optional<Categories> dbData = categoriesRepository.findById(id);
+	
+		//2.
+		if(dbData.isEmpty()) {
+			return "redirect:/categories";
+		}
+			
+		//3.
+		Categories categories = dbData.get();
+		model.addAttribute("categories", categories);
+		return "editCategories";
+	}
 	
 	
+	//＝＝ 更新 ＝＝
+		//更新ボタンを押したとき、itemsテーブルにデータを更新する
+		// ＊一連の流れ＊
+			//1. 更新対象のデータを取得
+			//2. 更新予定のデータが存在するか確認
+			//3. 取得できなかった場合、更新を行わずに、商品一覧の画面に戻る
+					//1.idを入れたオブジェクトを作成する
+					//  →DBから取得できたデータの特定のフィールドを書き換える
+					//2. 更新を実施
+			//4. DBのデータ更新を行う
+			//5. 商品一覧の画面を開く(リダイレクト)
+	@PostMapping("/category/{id}/edit")
+	public String categorieUupodate(
+			//パスパラ
+			@PathVariable(name="id")Integer id,
+			//リクエストパラメータ(入力フォーム)
+			@RequestParam(name="name", defaultValue="") String name
+			) {
+			
+			//1.
+			Optional<Categories> dbData = categoriesRepository.findById(id);
+			
+			//2.
+			if(!dbData.isEmpty()){
+				//3.
+				Categories categories = dbData.get();                 
+				categories.setName(name);
+				
+				//4. 更新を実施
+				categoriesRepository.save(categories);
+			}
+			
+			//5.
+			return "redirect:/categories";
+		}
+	
+		
+	//＝＝ 削除 ＝＝
+	//削除ボタンを押したとき、itemsテーブルにデータを更新する
+	// ＊一連の流れ＊
+		//1. 更新対象のデータを取得
+		//2. 更新予定のデータが存在するか確認
+		//3. 取得できたらデータを削除
+		//4. 商品一覧の画面を開く(リダイレクト)
+	@PostMapping("/categories/{id}/delete")
+	public String categoriesDelete(
+			@PathVariable(name="id")Integer id
+			) {
+			
+		//2.
+		Optional<Categories> dbData = categoriesRepository.findById(id);
+
+		//3.
+		if(!dbData.isEmpty()) {
+			categoriesRepository.deleteById(id);
+		}
+
+		//4.
+		return "redirect:/categories";
+	}
 }
